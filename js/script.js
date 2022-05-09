ea0019eec0eaa1e24f07e192a34fbce036827207
DmitryGolodnov <bestaxe1488@yandex.ru>
2022-05-09 20:18:42 +0300

    Edit header

--- js/script.js
+++ js/script.js
@@ -12,3 +12,48 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     $(event.target).focus();
   });
 });
+
+$(function() {
+  let header = $('.header_main');
+   
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 1) {
+     header.addClass('header_fixed');
+    } else {
+     header.removeClass('header_fixed');
+    }
+  });
+}); 
+
+
+$(function() {
+  let header = $('.header_main');
+  let hederHeight = header.height(); // вычисляем высоту шапки
+   
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 1) {
+     header.addClass('header_fixed');
+     $('body').css({
+        'paddingTop': hederHeight+'px' // делаем отступ у body, равный высоте шапки
+     });
+    } else {
+     header.removeClass('header_fixed');
+     $('body').css({
+      'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+     })
+    }
+    if($(this).scrollTop() > 940) {
+      header.css({
+        'opacity': '1'
+      });
+    } else {
+    header.css({
+      'opacity': '0.7'
+    });
+  }
+  });
+
+  
+});
+
+ 
